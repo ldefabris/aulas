4b4717c57bb328722ad167bf3b5abf46711c69ab
lfabris <profleolopes@gmail.com>
2015-05-29 11:39:46 -0300

    - Versão apenas com as anotações.

---
+++ src/Pessoa.java
@@ -0,0 +1,31 @@
+/**
+ * Classe responsável pela representação sistemica de pessoa. Esta classe deverá
+ * <strong>sempre</strong> ser instanciada no sistema
+ * 
+ * Variavéis do tipo <code> boolean</code> são <code>true or false</code>
+ * 
+ * <i>a documentação do java aceita formatação html</i>
+ * 
+ * <h2>E possui também suas próprias palavras reservadas:</h2>
+ * <p>
+ * As palavras reservadas nos comentários são denonimandas <i>annotations</i> e
+ * iniciam sempre pelo caractere: <b>'@'</>
+ * </p>
+ * <p>
+ * <i>Annotations</i> São responsáveis por descrever métodos, variavéis e
+ * classes, podendo também descrever seu comportamento a uma api. É possivél em
+ * tempo de execução recuperar uma annotation. Exemplo de annotations: Autor da
+ * classe:
+ * 
+ * @author Leonardo Lopes
+ *
+ */
+public class Pessoa {
+
+	/**
+	 * @serialData
+	 */
+	void teste() {
+
+	};
+}
