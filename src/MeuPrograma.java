17b31d91bebd999c6636134d1b83663120532655
lfabris <profleolopes@gmail.com>
2015-06-09 17:07:27 -0300

    Acessando atributos desprotegidos

---
+++ src/MeuPrograma.java
@@ -0,0 +1,9 @@
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa("João", 31, "M");
+		System.out.println("Atributos da pessoa:  " + p.genero);
+	}
+
+}
