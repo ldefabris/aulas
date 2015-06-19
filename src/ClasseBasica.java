354ff33080f54d9dbc47270931ae883dc48cdf71
lfabris <profleolopes@gmail.com>
2015-06-19 11:56:30 -0300

    Box e auto box exemplo

--- src/ClasseBasica.java
+++ src/ClasseBasica.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.TextFormatter;
+
 public class ClasseBasica {
 
 	@SuppressWarnings("all")
@@ -6,10 +8,11 @@ public class ClasseBasica {
 		numero = Double.valueOf(5.0);
 		double numeroPrimitivo = numero.longValue();
 		double numeroPrimitivoDeTexto = numero.valueOf("5");
-		Double resultado =  numero/8 + 3*numeroPrimitivo - numeroPrimitivoDeTexto;
-		System.out.println("O Resultado é" + resultado);
+		Double resultado = numero / 8 + 3 * numeroPrimitivo
+				- numeroPrimitivoDeTexto;
+		System.out.println("O Resultado é " + resultado);
+		Double salarioIdeal = Double.valueOf("Infinity");
+		System.out.println("O salario de desenv java deveria ser: "
+				+ salarioIdeal + " = " + salarioIdeal.longValue());
 	}
 }
-
-
-//Double numero = Double.valueOf("Infinity");
